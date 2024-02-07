179b82ced6b9d27d1e28d17002b08c0a52045fd2
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-02-07 15:04:38 -0300

    backup

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,12 +1,22 @@
 "use client";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { useEffect, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
-import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
+}
+
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
 }
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
@@ -15,19 +25,50 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   useEffect(() => {
     setOrderedData(data);
   }, [data]);
+
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) {
+      return;
+    }
+    //if dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // if user moves a list
+
+    if (type === "list") {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (item, index) => ({ ...item, order: index }));
+
+        setOrderedData(items);
+
+        //TODO: Trigger Server Action
+    }
+  };
   return (
-    <ol className="flex gap-x-3 h-full">
-        {orderedData.map((list, index) => {
-            return (
-                <ListItem
-                key={list.id}
-                index={index}
-                data={list}
-                />
-            )
-        })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -11,9 +11,10 @@ import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard, }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -61,7 +62,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
 
   const onBlur = () => {
     formRef.current?.requestSubmit();
-  }
+  };
 
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
@@ -74,10 +75,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start- gap-x-2">
       {isEditing ? (
-        <form 
-        ref={formRef}
-        action={handleSubmit}
-        className="flex-1 px-[2px]">
+        <form ref={formRef} action={handleSubmit} className="flex-1 px-[2px]">
           <input hidden id="id" name="id" value={data.id} />
           <input hidden id="boardId" name="boardId" value={data.boardId} />
           <FormInput
@@ -88,7 +86,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
             defaultValue={title}
             className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
           />
-          <button type="submit" hidden/>
+          <button type="submit" hidden />
         </form>
       ) : (
         <div
@@ -98,10 +96,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
-      <ListOptions
-      onAddCard={() =>{}}
-      data={data}
-      />
+      <ListOptions onAddCard={onAddCard} data={data} />
     </div>
   );
 };
