2e5988e24962736ab91c2c2459e030e433d4e92b
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-02-08 15:54:45 -0300

    copy list, copy card

--- actions/copy-list/index.ts
+++ actions/copy-list/index.ts
@@ -2,14 +2,14 @@
 
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+// import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 import { db } from "@/lib/db";
+import { createAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
 
 import { CopyList } from "./schema";
 import { InputType, ReturnType } from "./types";
-import { createAuditLog } from "@/lib/create-audit-log";
-// import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -73,12 +73,12 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     //   entityTitle: list.title,
     //   entityId: list.id,
     //   entityType: ENTITY_TYPE.LIST,
-    //   action: ACTION.DELETE,
+    //   action: ACTION.CREATE,
     // })
   } catch (error) {
     return {
-      error: "Failed to copy.",
-    };
+      error: "Failed to copy."
+    }
   }
 
   revalidatePath(`/board/${boardId}`);
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,9 +1,19 @@
+import { Toaster } from "sonner";
+
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-import {Toaster} from "sonner"
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>
-    <Toaster/> {children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
